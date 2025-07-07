89df5a82c75fcc13b99459096c5112b0caef84cf
ShahnozaTT <shturabova@gmail.com>
2025-07-07 18:06:36 +0500

    Add files via upload

---
+++ lesson-1/homework/lesson-1.py
@@ -0,0 +1,46 @@
+#Homework
+#1. Given a side of square. Find its perimeter and area.
+
+
+side = 5 
+perimeter = 4 * side
+area = side * side
+
+print("Kvadratning perimetri:", perimeter)
+print("Kvadratning yuzi:", area)
+
+
+#2. Given diameter of circle. Find its length.
+
+diameter = 15
+pi = 3.14
+length = pi * diameter
+
+print("Doiraning uzunligi:", length)
+
+
+
+#3.Given two numbers a and b. Find their mean.
+
+a=10
+b=30
+mean = (a + b) / 2
+
+print("Sonlarning o'rtacha qiymati:", mean) 
+
+#4. Given two numbers a and b. Find their sum, product and square of each number.
+
+a = 54
+b = 24  
+sum_ab = a + b
+product_ab = a * b
+square_a = a ** 2
+square_b = b ** 2
+
+print("Sonlarning yig'indisi:", sum_ab)
+print("Sonlarning ko'paytmasi:", product_ab)
+print("Birinchi sonning kvadrati:", square_a)
+print("Ikkinchi sonning kvadrati:", square_b)        
+
+
+
